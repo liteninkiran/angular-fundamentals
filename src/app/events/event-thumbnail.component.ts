182ac71d9600bad58e5edfd7fcadb60cf39b2328
Kiran Anand <liteninkiran@hotmail.com>
2021-04-13 16:26:05 +0100

    Styling Components with ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,6 +9,8 @@ import { Component, Input } from '@angular/core';
         .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
+        .green { color: #003300 !important }
+        .bold { font-weight: bold; }
 
     `]
 })
@@ -16,4 +18,28 @@ import { Component, Input } from '@angular/core';
 export class EventThumbnailComponent
 {
     @Input() eventObj:any;
+
+    getStartTimeClass()
+    {
+        const isEarlyStart = this.eventObj && this.eventObj.time === '8:00 am';
+
+        let classesArr = [];
+        let classes = '';
+
+        if(isEarlyStart)
+        {
+            classes = 'green bold';
+            classesArr = ['green', 'bold'];
+        }
+
+        // Return as string
+        return classes;
+
+        // Return as array of strings
+        //return classesArr;
+
+        // Return as object
+        //return {green: isEarlyStart, bold: isEarlyStart};
+
+    }
 }
