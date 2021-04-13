d686046a9f4f9358a3e1ecb92abcbb842426a473
Kiran Anand <liteninkiran@hotmail.com>
2021-04-13 15:25:33 +0100

    Repeating data with ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,40 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component(
 {
     selector: 'event-thumbnail',
-    template: `
-        <div class="well hoverwell thumbnail">
-
-            <h2>{{ event.name }}</h2>
-
-            <div>Date: {{ event.date }}</div>
-            <div>Time: {{ event.time }}</div>
-            <div>Price: £{{ event.price }}</div>
-
-            <div>
-            
-                <span>Location: {{ event.location.address }}</span>
-                <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
-            
-            </div>
-
-        <div>    
-    `,
+    templateUrl: './event-thumbnail.component.html',
     styles: [`
     
-        .pad-left
-        {
-            margin-left: 10px;
-        }
-
-        .well div
-        {
-            color: #bbb;
-        }
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
 
     `]
 })
 
 export class EventThumbnailComponent
 {
-    @Input() event:any;
+    @Input() eventObj:any;
 }
