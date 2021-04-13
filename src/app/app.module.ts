923944da67224e19adb7c1ae233adb94aab5bcfa
Kiran Anand <liteninkiran@hotmail.com>
2021-04-13 14:46:26 +0100

    Adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule(
 {
@@ -11,7 +12,8 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component';
     [
         EventsAppComponent,
         EventsListComponent,
-        EventThumbnailComponent
+        EventThumbnailComponent,
+        NavBarComponent
     ],
     imports:
     [
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'events-app',
-  template: '<events-list></events-list>',
+@Component(
+{
+    selector: 'events-app',
+    template: `
+        <nav-bar></nav-bar>
+        <events-list></events-list>
+    `,
 })
-export class EventsAppComponent {
-  title = 'ng-fundamentals';
+
+export class EventsAppComponent
+{
+    
 }
