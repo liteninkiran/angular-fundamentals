0603de6e086d2ebc345552ab4fd94ec5d165c9b8
Kiran Anand <liteninkiran@hotmail.com>
2021-04-13 10:11:58 +0100

    Basic data-bound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
-@NgModule({
-  declarations: [
-    EventsAppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [EventsAppComponent]
+@NgModule(
+{
+    declarations:
+    [
+        EventsAppComponent,
+        EventsListComponent
+    ],
+    imports:
+    [
+        BrowserModule
+    ],
+    bootstrap:
+    [
+        EventsAppComponent
+    ]
 })
+
 export class AppModule { }
