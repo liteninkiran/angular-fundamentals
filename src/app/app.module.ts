6a1364b537fcceb4da5c8e7dcf5c3fbfd55b5ecf
Kiran Anand <liteninkiran@hotmail.com>
2021-04-14 10:15:58 +0100

    Implementing a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule(
 {
@@ -22,7 +23,11 @@ import { NavBarComponent } from './nav/navbar.component';
     bootstrap:
     [
         EventsAppComponent
-    ]
+    ],
+    providers:
+    [
+        EventService
+    ],
 })
 
 export class AppModule { }
