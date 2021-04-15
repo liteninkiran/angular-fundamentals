4ec43ec6525a23618002448437f53b024542de65
Kiran Anand <liteninkiran@hotmail.com>
2021-04-15 17:08:35 +0100

    Creating 'new event' form (reactive based)

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventService } from './shared/event.service';
 
 @Component(
 {
     templateUrl: './create-event.component.html',
+    styles:
+    [`
+        em { float:right; color:#E05C65; padding-left:10px; }
+        .error input { background-color: #E3C3C5; }
+
+        .error ::-webkit-input-placeholder { color:#999; }  /*   Safari / iOS   */
+        .error ::-moz-placeholder { color:#999; }           /*   Mozilla 19+    */
+        .error :-moz-placeholder { color:#999; }            /*   Mozilla 18-    */
+        .error :ms-input-placeholder { color:#999; }        /*   IE 10+         */
+    `]
 })
 
 export class CreateEventComponent
 {
+    newEvent
 
     isDirty:boolean = true;
 
-    constructor(private router:Router)
+    constructor(private eventService: EventService, private router:Router)
     {
 
+    }
+
+    saveEvent(formValues)
+    {
+        this.eventService.saveEvent(formValues);
+        this.isDirty = false;
+        this.router.navigate(['/events']);
     }
 
     cancel()
