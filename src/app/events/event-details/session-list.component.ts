4f9f88c444b8176e8548b2c57754141f2f720334
Kiran Anand <liteninkiran@hotmail.com>
2021-04-16 16:25:52 +0100

    Sorting data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -11,6 +11,7 @@ export class SessionListComponent implements OnChanges
 {
     @Input() sessions:ISession[];
     @Input() filterBy:string;
+    @Input() sortBy:string;
 
     visibleSessions: ISession[] = [];
 
@@ -19,6 +20,7 @@ export class SessionListComponent implements OnChanges
         if(this.sessions)
         {
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesAsc);
         }
     }
 
@@ -36,4 +38,16 @@ export class SessionListComponent implements OnChanges
                 });
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession)
+{
+    if(s1.name > s2.name) { return 1; }
+    else if(s1.name === s2.name) { return 0; }
+    else { return -1; }
+}
+
+function sortByVotesAsc(s1: ISession, s2: ISession)
+{
+    return s2.voters.length - s1.voters.length;
 }
