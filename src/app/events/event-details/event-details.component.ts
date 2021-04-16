e62c997136d576d6c99ec099db836fac59fe1618
Kiran Anand <liteninkiran@hotmail.com>
2021-04-16 15:58:12 +0100

    Displaying filter values

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -19,6 +19,8 @@ export class EventDetailsComponent
 
     event:IEvent;
     addMode:boolean;
+    filterBy: string = 'all';
+    sortBy: string = 'votes';
 
     constructor(private eventService:EventService, private route:ActivatedRoute)
     {
