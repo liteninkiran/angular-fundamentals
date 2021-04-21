6307fa6b97e2f951745da82a72cc9d5cd834933d
Kiran Anand <liteninkiran@hotmail.com>
2021-04-21 17:14:33 +0100

    Persisting authentication status across page refreshes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './user/auth.service';
 
 @Component(
 {
@@ -11,5 +12,13 @@ import { Component } from '@angular/core';
 
 export class EventsAppComponent
 {
-    
+    constructor(private auth:AuthService)
+    {
+
+    }
+
+    ngOnInit()
+    {
+        this.auth.checkAuthenticationStatus();
+    }
 }
